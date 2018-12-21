302c4d0a84d74fdd2ff2549654f3becf0b57e523
ip1259 <43847161+ip1259@users.noreply.github.com>
2018-12-22 02:24:12 +0800

    update
    1.
    Selection
    implements comparable
    for sort
    add: a new variable score
            three new methods clacScore(Selection setting), getScore(), compareTo(Selection other)
    2.
    SelectionGroup
    add a new method sortSelections()
    sort by score

--- src/Selection.java
+++ src/Selection.java
@@ -1,43 +1,76 @@
-package Selection;
+package com.example.shockdotjava.selecthelper;
 
+import android.content.Intent;
+
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Random;
 
-public class Selection
+public class Selection implements Comparable<Selection>
 {
 	private String name;
 	private ArrayList<String> Answers;
 	private int selectedTimes;
-	
+	private int score;
+
 	public Selection(String name)
 	{
 		this.name = name;
 		Answers = new ArrayList<>();
 		this.selectedTimes = 0;
 	}
-	
+
 	public void beenSelected()
 	{
 		this.selectedTimes++;
 	}
-	
+
+	public int getSelectedTimes()
+	{
+		return this.selectedTimes;
+	}
+
 	public String SelectionName()
 	{
 		return this.name;
 	}
-	
+
 	public String getAnswerOf(int index)
 	{
 		return this.Answers.get(index);
 	}
-	
+
 	public void setAnswerOf(int index, String answer)
 	{
 		this.Answers.set(index, answer);
 	}
-	
+
 	public void addAnswerToSelection(String answer)
 	{
 		this.Answers.add(answer);
 	}
-	
+
+	public ArrayList<String> getAnswers()
+	{
+		return this.Answers;
+	}
+
+	public int getScore()
+	{
+		return this.score;
+	}
+
+	@Override
+	public int compareTo(Selection other) {
+		return this.score - other.score;
+	}
+
+	//-----------------unfinished-------------------------------
+
+	public void clacScore(Selection setting)
+	{
+		Random random = new Random();
+		this.score = random.nextInt();
+	}
+
 }
